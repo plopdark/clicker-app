c917825e1e930444d17a6baa034214712b7dfc7e
Paul <ploppasha@gmail.com>
2024-02-26 19:07:18 +0200

    basic login functionality

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,10 +4,12 @@ import { AccountComponent } from './pages/account/account.component';
 import { TopListComponent } from './pages/top-list/top-list.component';
 import { ShopComponent } from './pages/shop/shop.component';
 import { RoutingEnum } from './utilities/enums/routing.enum';
+import { LoginComponent } from './pages/login/login.component';
 
 export const routes: Routes = [
   { path: '', component: MainComponent },
   { path: RoutingEnum.Account, component: AccountComponent },
   { path: RoutingEnum.TopList, component: TopListComponent },
   { path: RoutingEnum.Shop, component: ShopComponent },
+  { path: 'login', component: LoginComponent },
 ];
