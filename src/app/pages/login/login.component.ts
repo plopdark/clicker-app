5a2e93d82c3aba4250d802723ffb447e8d627d67
Paul <ploppasha@gmail.com>
2024-02-27 19:40:13 +0200

    added auth formgroup

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,17 +1,39 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  Validators,
+  ReactiveFormsModule,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss',
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  public loginForm = new FormGroup({
+    name: new FormControl(''),
+    email: new FormControl('', [Validators.email]),
+    password: new FormControl('', [
+      Validators.minLength(6),
+      Validators.required,
+    ]),
+    repeatPassword: new FormControl(''),
+    rememberMe: new FormControl<boolean>(false),
+  });
   public isSignUp: boolean = false;
 
   constructor() {}
+
+  ngOnInit(): void {}
+
+  public onSubmit() {
+    console.log(this.loginForm.value);
+  }
 
   public onSignUp(): void {
     this.isSignUp = true;
